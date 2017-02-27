2c3d98bb565a746376b0334553d4135e90298e00
aron0093 <aron0093@gmail.com>
2017-02-27 22:16:39 +0100

    Used LinearSVC instead of SVC, dramatic shortening of time. Other methods added as comments. Testing required.

--- SignalP/scripts/cross_val_score.py
+++ SignalP/scripts/cross_val_score.py
@@ -7,29 +7,53 @@ Created on Tue Feb 21 21:00:20 2017
 
 import SS_parser
 import time
+import numpy as np
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
 
 filepath = '''/KB8024/KB8024/data/globular_signal_tm_3state.txt'''
 window_size = 3
 
+print("Parsing data...")
+
 X, Y = SS_parser.skl_parser(filepath, window_size)
 
-print("Data parsing successful")
-
-from sklearn.svm import SVC
-from sklearn.model_selection import cross_val_score
-
-print("Starting cross validation")
+print("Scoring over cross validation sets...")
 
 start = time.time()
 
-score = cross_val_score(SVC(kernel ='linear'), X, Y, cv=2)
+score = cross_val_score(LinearSVC(), X, Y, cv=3)
 
 end = time.time()
 
-print ("Time taken for cross validation %f"%(end-start))
-print('\n')
-print(score)
-print('\n')
-print("Accuracy: %0.2f (+/- %0.2f)" % (score.mean(), score.std() * 2))
+print ("Time taken for cross validation scoring : %f"%(end-start))
+print("Individual scores: ",score)
+print("Overall Accuracy: %0.2f (+/- %0.2f)" % (score.mean(), score.std() * 2))
+
+#start = time.time()
+#clf = OneVsRestClassifier(SVC(kernel='linear', probability=True, class_weight='auto'))
+#clf.fit(X, y)
+#end = time.time()
+#print "Single SVC", end - start, clf.score(X,y)
+#proba = clf.predict_proba(X)
+
+#n_estimators = 10
+#start = time.time()
+#clf = OneVsRestClassifier(BaggingClassifier(SVC(kernel='linear', probability=True, class_weight='auto'), max_samples=1.0 / n_estimators, #n_estimators=n_estimators))
+#clf.fit(X, y)
+#end = time.time()
+#print "Bagging SVC", end - start, clf.score(X,y)
+#proba = clf.predict_proba(X)
+
+#start = time.time()
+#clf = RandomForestClassifier(min_samples_leaf=20)
+#clf.fit(X, y)
+#end = time.time()
+#print "Random Forest", end - start, clf.score(X,y)
+#proba = clf.predict_proba(X)
+
 
 
